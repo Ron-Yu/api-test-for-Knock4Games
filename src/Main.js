0d448f92ecf00d7203af7d778cb6421c32509f2a
Ron Yu <ronyu77@gmail.com>
2017-02-08 00:03:06 +0800

    feat: Add Main component as container

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Login from './Login';
-import MemberList from './MemberList'
-import CreateMember from './CreateMember'
+import MemberList from './MemberList';
+import CreateMember from './CreateMember';
 
 class Main extends Component {
   constructor() {
@@ -11,64 +11,165 @@ class Main extends Component {
     this.fetchAccessToken = this.fetchAccessToken.bind(this);
     this.fetchMemberList = this.fetchMemberList.bind(this);
     this.createNewMember = this.createNewMember.bind(this);
-    this.onInputChange = this.onInputChange.bind(this);
+    this.mapInputValueToState = this.mapInputValueToState.bind(this);
+    this.resetMsg = this.resetMsg.bind(this);
+    this.checkToken = this.checkToken.bind(this);
 
     this.state = {
+      USER_NAME_PROPERTY: 'userNameValue',
+      USER_PWD_PROPERTY: 'userPwdValue',
+      NEW_USER_NAME_PROPERTY: 'newUserNameValue',
+      userNameValue: '',
+      userPwdValue: '',
+      newUserNameValue: '',
       memberList: [],
-      userName: '',
-      userPwd: '',
-      newMemberName: ''
+      isLoginFailed: false,
+      isLoginSuccessed: false,
+      isTokenExisted: false
     };
+  }
+
+  componentDidMount() {
+    this.checkToken()
   }
 
   render() {
     const {
       state: {
-        userName,
-        userPwd,
+        USER_NAME_PROPERTY,
+        USER_PWD_PROPERTY,
+        NEW_USER_NAME_PROPERTY,
+        userNameValue,
+        userPwdValue,
+        newUserNameValue,
         memberList,
-        newMemberName
+        isLoginFailed,
+        isLoginSuccessed,
+        isTokenExisted
       },
       fetchAccessToken,
       fetchMemberList,
       createNewMember,
-      onInputChange
+      mapInputValueToState,
+      resetMsg,
+      checkToken
     } = this;
 
     return (
-      <main>
-        <Login name={userName} pwd={userPwd} onSubmit={fetchAccessToken} onInputChange={onInputChange}/>
-        <MemberList onSubmit={fetchMemberList}  />
-        <CreateMember onSubmit={createNewMember} />
+      <main style={{
+        display: 'flex',
+        justifyContent: 'space-around'
+      }}>
+        <Login
+          USER_NAME_PROPERTY={USER_NAME_PROPERTY}
+          USER_PWD_PROPERTY={USER_PWD_PROPERTY}
+          name={userNameValue}
+          pwd={userPwdValue}
+          isLoginFailed={isLoginFailed}
+          isLoginSuccessed={isLoginSuccessed}
+          onSubmit={fetchAccessToken}
+          onInputChange={mapInputValueToState}
+          resetMsg={resetMsg}
+          checkToken={checkToken}
+        />
+        <MemberList
+          onSubmit={fetchMemberList}
+          resetMsg={resetMsg}
+          memberList={memberList}
+          isTokenExisted={isTokenExisted}
+        />
+        <CreateMember
+          onSubmit={createNewMember}
+          fetchMemberList={fetchMemberList}
+          onInputChange={mapInputValueToState}
+          newUserNameValue={newUserNameValue}
+          isTokenExisted={isTokenExisted}
+          NEW_USER_NAME_PROPERTY={NEW_USER_NAME_PROPERTY}
+        />
       </main>
     );
   }
 
   fetchAccessToken(name, pwd) {
-    const PATH = 'http://54.64.163.121:3443';
+    return new Promise((resolve, reject) => {
+      const URL = 'http://54.64.163.121:3443';
 
-    axios
-      .post(PATH, {
-        name,
-        pwd
-      })
-      .then(({data}) => {
-        console.log(data)
-        localStorage.setItem('token', data.token.token)
-      })
+      axios
+        .post(URL, {
+          name,
+          pwd
+        })
+        .then(({data}) => {
+          localStorage.setItem('token', data.token.token)
+          this.setState({isLoginSuccessed: true})
+          resolve()
+        })
+        .catch(err => {
+          this.setState({isLoginFailed: true})
+          throw err
+        })
+    })
   }
 
   fetchMemberList() {
+    return new Promise((resolve, reject) => {
+      const URL = 'http://54.64.163.121:3443/member';
+      const TOKEN = localStorage.getItem('token');
 
+      axios({
+        url: URL,
+        headers: {'Authorization': TOKEN}
+      })
+        .then(({data}) => {
+          this.setState({
+            memberList: data.data.reverse()
+          })
+          resolve()
+        })
+        .catch(err => {
+          throw err
+        })
+    })
   }
 
-  createNewMember() {
+  createNewMember(name) {
+    return new Promise((resolve, reject) => {
+      const URL = 'http://54.64.163.121:3443/member';
+      const TOKEN = localStorage.getItem('token');
 
+      axios({
+        url: URL,
+        method: 'post',
+        headers: {'Authorization': TOKEN},
+        data: {
+          name
+        }
+      })
+        .then(response => {
+          resolve()
+        })
+        .catch(err => {
+          throw err
+        })
+    })
   }
 
-  onInputChange(property, value) {
+  mapInputValueToState(property, value) {
     this.setState({
       [property]: value
+    })
+  }
+
+  resetMsg() {
+    this.setState({
+      isLoginFailed: false,
+      isLoginSuccessed: false
+    })
+  }
+
+  checkToken() {
+    this.setState({
+      isTokenExisted: localStorage.getItem('token') ? true : false
     })
   }
 }
