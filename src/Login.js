93b30ce78f22e4069476acadcf94de32b3f88fc3
Ron Yu <ronyu77@gmail.com>
2017-02-08 00:03:42 +0800

    feat: Add Login component

--- src/Login.js
+++ src/Login.js
@@ -1,48 +1,86 @@
 import React, { PropTypes } from 'react';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+import Snackbar from 'material-ui/Snackbar';
+import Paper from 'material-ui/Paper';
 
 const propTypes = {
+  USER_NAME_PROPERTY: PropTypes.string.isRequired,
+  USER_PWD_PROPERTY: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   pwd: PropTypes.string.isRequired,
   onSubmit: PropTypes.func.isRequired,
-  onInputChange: PropTypes.func.isRequired
-}
+  onInputChange: PropTypes.func.isRequired,
+  resetMsg: PropTypes.func.isRequired,
+  isLoginFailed: PropTypes.bool.isRequired,
+  isLoginSuccessed: PropTypes.bool.isRequired,
+  checkToken: PropTypes.func.isRequired
+};
 
 const Login = (props) => {
   const {
     name,
     pwd,
     onSubmit,
-    onInputChange
+    onInputChange,
+    USER_NAME_PROPERTY,
+    USER_PWD_PROPERTY,
+    isLoginFailed,
+    isLoginSuccessed,
+    resetMsg,
+    checkToken
   } = props;
 
   return (
-    <section>
+    <Paper
+      zDepth={3}
+      style={{
+      flex: '0 1 30%',
+      padding: 20
+    }}>
+      <h2>使用者註冊區</h2>
       <TextField
         floatingLabelText="name"
         value={name}
-        data-input-property="userName"
+        name={USER_NAME_PROPERTY}
         onChange={handleInputChange}
       />
       <br />
       <TextField
         floatingLabelText="password"
         value={pwd}
-        data-input-property="userPwd"
+        name={USER_PWD_PROPERTY}
         onChange={handleInputChange}
       />
       <br />
-      <RaisedButton label="Default" onClick={handleSubmit} />
-    </section>
+      <RaisedButton label="登入" onClick={handleSubmit} primary={true} disabled={isInputEmpty()}/>
+      <Snackbar
+        open={isLoginFailed}
+        message='錯誤發生，請重新輸入'
+      />
+      <Snackbar
+        open={isLoginSuccessed}
+        message='登入成功'
+      />
+    </Paper>
   );
 
   function handleSubmit() {
-    onSubmit(name, pwd);
+    onSubmit(name, pwd)
+      .then(() => {
+        onInputChange(USER_NAME_PROPERTY, '')
+        onInputChange(USER_PWD_PROPERTY, '')
+        checkToken()
+      })
   }
 
   function handleInputChange(e, value) {
-    onInputChange(e.target.dataset.inputProperty, value)
+    resetMsg()
+    onInputChange(e.target.name, value)
+  }
+
+  function isInputEmpty() {
+    return name === '' || pwd === ''
   }
 }
 
